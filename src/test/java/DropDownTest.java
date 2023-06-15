a9f7cb4f6abbe0603ac19b283dcf002e7fb09156
Ivan <ishkredov2@gmail.com>
2023-06-15 17:50:35 +0300

    Initial commit

---
+++ src/test/java/DropDownTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class DropDownTest extends BaseTest {
+
+    @Test
+    public void checkDropDown() {
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        WebElement dropdown = driver.findElement(By.id("dropdown"));
+        Select select = new Select(dropdown);
+        List options = select.getOptions();
+
+        assertEquals(options.size(),3);
+        select.selectByIndex(1);
+        assertEquals(select.getFirstSelectedOption().getText(),"Option 1");
+
+        select.selectByIndex(2);
+        assertEquals(select.getFirstSelectedOption().getText(),"Option 2");
+
+
+    }}
